6f5394bd876fc8a50288671e483900231a7da708
qiru <bjqiru@corp.netease.com>
2018-08-24 18:11:46 +0800

    添加开始动画

--- public/scripts/pages/rongge.js
+++ public/scripts/pages/rongge.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import {guide, understand, intro, firstQuestion} from 'pages/rongge/jsondata';
 
 class RonggeTest{
    constructor(){
@@ -6,8 +7,6 @@ class RonggeTest{
       this.scale = window.innerWidth / 750;
       this.height = window.innerHeight/ this.scale;
       this.loader = PIXI.loader;
-      this.operationFlag = false;
-      
       //Ae
       this.mainContainer = new PIXI.Container();
       this.mainContainer.width = this.width;
@@ -16,6 +15,13 @@ class RonggeTest{
       this.endContainer = new PIXI.Container();
       this.endContainer.width = this.width;
       this.endContainer.height = this.height;
+
+      this.bgContainer = new PIXI.Container();//rt
+      this.operationFlag = false;
+      this.positionY = 0;//jt
+      this.curStage = "start_guide";//ct
+      this.comparePosition = 0;//kt
+ 
       this.imgSrc = '/images/rongge/';
       $('#content').css({
          "-webkit-transform": "rotate(0deg) scale(" + this.scale + ") translate3d(0,0,0)",
@@ -79,7 +85,7 @@ class RonggeTest{
    }
    loaderMain = () => {
       $("#loading").fadeOut();
-      this.bgContainer = new PIXI.Container();//rt
+      
       let bg = this.renderRect({
          color: 16119285,
          width: 750,
@@ -88,15 +94,18 @@ class RonggeTest{
          y: 0
       })
       this.bgContainer.addChild(bg);
+      //添加内容
+      this.addContent();
+
       this.mainContainer.addChild(this.bgContainer);
       this.mainView.render(this.mainContainer);
       this.endView.render(this.endContainer);
       this.bgContainer.interactive = true;
       this.bgContainer.buttonMode = true;
-      console.log(this.scroller)
-      this.scroller.setDimensions(this.width, this.height, this.width, 0);
-      this.updateLoop();
+      this.scroller.setDimensions(this.width, this.height, this.width, this.height);
       this.bindEvent();
+      this.updateLoop();
+      
 
    }
    bindEvent(){
@@ -142,6 +151,218 @@ class RonggeTest{
    scrollFun = (left, top, bottom) => {
       console.log(left, top, bottom)
       this.bgContainer.position.y = -2 * top
+   }
+
+   renderJsonAnimate(jsonData, jsonFile){//c
+      let jsonContainer = new PIXI.Container();//i
+      let jsonContainerHeight = 0;//a
+      //let jsonPosition = 0;//o
+      for(var i = 0; i < jsonData.length; i++){
+         let itemContainer = new PIXI.Container();//r
+         let sprite;
+         if(jsonFile){
+            sprite = new PIXI.Sprite(PIXI.loader.resources[this.imgSrc + jsonFile].textures[jsonData[i].bg])
+         }else{
+            sprite = new PIXI.Sprite(this.loader.resources[this.imgSrc + jsonData[i].bg].texture)
+         }
+         itemContainer.addChild(sprite);
+         if(jsonData[i].content){
+            let content = jsonData[i].content;
+            for(var j = 0; j < content.length; j++){
+               let contentItem = content[j];//h
+               let contentSprite;//e
+               let itemTween, itemTween1, itemTween2, itemTween3;
+               if("image" == contentItem.type){
+                  if(jsonFile){
+                     contentSprite = new PIXI.Sprite(PIXI.loader.resources[this.imgSrc + jsonFile].textures[contentItem.url])
+                  }else{
+                     contentSprite = PIXI.Sprite.fromImage(this.imgSrc + contentItem.url)
+                  }
+                  if(content.length > 1){
+                     contentSprite.position.set(40 + 50 * j, contentItem.positionY);
+                  }else{
+                     contentSprite.pivot.set(28, 28);
+                     contentSprite.position.set(118, contentItem.positionY + 28);
+                     itemTween = new TWEEN.Tween({
+                        rotation: 0
+                     }).to({
+                        rotation: -15 / 180 * Math.PI
+                     }, 140).onUpdate(function () {
+                        contentSprite.rotation = this.rotation
+                     });
+                     itemTween1 = new TWEEN.Tween({
+                        rotation: -15 / 180 * Math.PI
+                     }).to({
+                        rotation: 0
+                     }, 140).onUpdate(function () {
+                        contentSprite.rotation = this.rotation
+                     });
+                     itemTween2 = new TWEEN.Tween({
+                        rotation: 0
+                     }).to({
+                        rotation: -15 / 180 * Math.PI
+                     }, 140).onUpdate(function () {
+                        contentSprite.rotation = this.rotation
+                     });
+                     itemTween3 = new TWEEN.Tween({
+                        rotation: -15 / 180 * Math.PI
+                     }).to({
+                        rotation: 0
+                     }, 140).onUpdate(function () {
+                        contentSprite.rotation = this.rotation
+                     })
+                     contentSprite.tween = itemTween;
+                     itemTween.chain(itemTween1);
+                     itemTween1.chain(itemTween2);
+                     itemTween2.chain(itemTween3);
+                  }
+               }else if("text_image" == contentItem.type){
+                  contentSprite = new PIXI.Sprite(PIXI.loader.resources[this.imgSrc + jsonFile].textures[contentItem.url]);
+                  contentSprite.position.set(40, contentItem.positionY)
+               }
+               itemContainer.addChild(contentSprite);
+            }
+         }
+         itemContainer.duration = jsonData[i].duration;
+         itemContainer._height = itemContainer.height - 17;
+         itemContainer.pivot.set(14, itemContainer.height - 23);
+         itemContainer.position.set(19, 30 + jsonContainerHeight + itemContainer.height - 23);
+         //jsonPosition += itemContainer.height - 17;
+         jsonContainerHeight += itemContainer._height;
+         itemContainer.scale.set(0);
+         jsonContainer.addChild(itemContainer)
+      }
+      jsonContainer._height = jsonContainerHeight;
+      this.contentContainer.addChild(jsonContainer);
+   }
+   addContent(){//o
+      this.contentContainer = new PIXI.Container();//Ke;
+      this.renderJsonAnimate(guide, "animate/guide.json");
+      this.renderJsonAnimate(understand, "animate/guide.json");
+      this.renderJsonAnimate(intro, "animate/q1.json");
+      this.renderJsonAnimate(firstQuestion, "animate/q1.json");
+
+      this.btnContainer = new PIXI.Container();//Ze
+      this.btnContainer.position.set(0, this.height);
+      this.addBtnContent(); 
+      this.showJsonAnimate(this.contentContainer.children[0]);
+      setTimeout(() => {
+         this.contentContainer.children[0].children[0].children[1].tween.start()
+     }, 800),
+
+
+      this.bgContainer.addChild(this.contentContainer);
+      
+   }
+
+   showJsonAnimate(jsonContainer, num){//h
+      console.log(jsonContainer)
+      jsonContainer.position.set(0, this.positionY);
+      let arr = [];
+      if(num){
+         let height = 0;
+         jsonContainer.children.forEach(function (item, index) {
+            if(index >= num){
+               jsonContainer.position.set(19, 30 + height + jsonContainer._height + 17 - 23);
+               height += jsonContainer._height;
+            }
+         })
+      }else{
+         num = 0;
+      }
+      for(let i = num; i < jsonContainer.children.length; i++){
+         arr.push(()=>{
+            let timer = null;
+            let promise = new Promise((resolve, reject)=>{
+               this.showTweenAnimate(jsonContainer.children[i]);
+               timer = setTimeout(() => {
+                  resolve();
+               }, jsonContainer.children[i].duration || 1e3);
+               
+            })
+            promise.timer = timer;
+            return promise;
+         })
+      }
+      this.forFun(arr).then(()=>{
+         this.positionY += 30;
+         //"startGuide" == ct ? x(Ze.children[0]) : "guide1" == ct ? x(Ze.children[1]) : "q1" == ct ? x(Ze.children[2]) : "guide_end2" == ct ? q() : "q2" == ct ? S() : "q3" == ct ? B() : "q4" == ct ? U() : "q5" == ct ? X() : "q6" == ct ? R() : "q6_state2" == ct ? F() : "input" == ct ? J() : "end" == ct && G()
+      })
+   }
+
+   forFun(arr){//l
+      let resolve = Promise.resolve();
+      arr.forEach((e) => {
+         resolve = resolve.then(e);
+      });
+      return resolve;
+   }
+
+   showTweenAnimate(subContainer, num){//u
+      let bigTween = new TWEEN.Tween({
+         scale: 0
+      }).to({
+         scale: 1.05
+      }, 280).onUpdate(function(){
+         subContainer.scale.set(this.scale);
+      }).start();
+      let smallTween = new TWEEN.Tween({
+         scale: 1.05
+      }).to({
+         scale: 1
+      }, 220).onUpdate(function(){
+         subContainer.scale.set(this.scale);
+      });
+      bigTween.chain(smallTween);
+      let height = subContainer._height;
+      this.positionY += height;
+      if("q4_state2" != this.curStage && "q6_state2" != this.curStage){
+         if(!num){
+            if(this.comparePosition <= subContainer.position.y - height){
+               if("guide2" == this.curStage){
+                  height += 90;
+               }
+               if('q1' == this.curStage){
+                  height += 50;
+               }
+               if('q2' == this.curStage){
+                  height += 140;
+               }
+               if('q3' == this.curStage){
+                  height += 140;
+               }
+               if('q4' == this.curStage){
+                  height += 145;
+               }
+               if('q5' == this.curStage){
+                  height += 145;
+               }
+               if('q6' == this.curStage){
+                  height += 170;
+               }
+               if('end' == this.curStage){
+                  height += 50;
+               }
+               this.showQ3Animate(height);
+            }else if(this.comparePosition < subContainer.position.y){
+               this.showQ3Animate(subContainer.parent.position.y - this.comparePositiont)
+            }
+         }
+      }
+   }  
+   showQ3Animate(height, time){
+      this.comparePosition += height;
+      // Ze.children.forEach(function (e) {
+      //    e.chosen || e.position.set(0, kt)
+      // });
+     new TWEEN.Tween(this.bgContainer.position).to({
+         y: -this.comparePosition
+     }, time || 300).onUpdate(function () {
+         //st.canMove || (st.position.y = -rt.position.y)
+     }).start()
+   }
+   addBtnContent(){//g()
+      
    }
 }
 
---
+++ public/scripts/pages/rongge/jsondata.js
@@ -0,0 +1,64 @@
+exports.guide = [{
+      bg: "rect_1.png",
+      content: [{
+         type: "image",
+         url: "hand1.png",
+         positionY: 26
+      }]
+   }, {
+      bg: "rect_2.png",
+      duration: 1500
+   }, {
+      bg: "rect_3.png",
+      content: [{
+         type: "image",
+         url: "hand2.png",
+         positionY: 26
+      }, {
+         type: "image",
+         url: "hand2.png",
+         positionY: 26
+      }, {
+         type: "image",
+         url: "hand2.png",
+         positionY: 26
+      }]
+   }, {
+      bg: "rect_4.png",
+      duration: 1500
+   }, {
+      bg: "rect_5.png"
+   }, {
+      bg: "rect_6.png",
+      duration: 1e3
+   }];
+
+exports.understand = [{
+      bg: "rect_7.png"
+   }, {
+      bg: "rect_8.png"
+   }, {
+      bg: "rect_9.png"
+   }, {
+      bg: "rect_10.png"
+   }]
+
+exports.intro = [{
+      bg: "d0.png"
+   }, {
+      bg: "d10.png"
+   }, {
+      bg: "d1.png"
+   }, {
+      bg: "d2.png"
+   }, {
+      bg: "d3.png"
+   }, {
+      bg: "d4.png"
+   }];
+
+exports.firstQuestion = [{
+      bg: "d7.png"
+   }, {
+      bg: "d8.png"
+   }]
